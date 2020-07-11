cdea0236670fa866535aca116d0d5b533d9cc95a
里千晴 <satochiharu@satonoMacBook-Pro.local>
2020-07-11 16:43:10 +0900

    [add]オブジェクトを使用して時刻を表示

---
+++ sample_04.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+// タイムゾーンを東京標準に設定する
+date_default_timezone_set('Asia/Tokyo');
+// dateタイムメソッド（日付や時間に関するメソッド）をtoday変数に代入する
+$today = new DateTime();
+// 時刻をフォーマットメソッドに沿って画面に表示する
+print($today -> format('G時 i分 s秒 '));
+?>
+</pre>
+</main>
+</body>
+</html>
