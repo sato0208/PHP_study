649e679a401c0bd8b57ed82721211be38cddf4e9
sato <kujira028@gmail.com>
2020-07-11 18:08:12 +0900

    [update]明日の日付を表示させる

---
+++ sample_07.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+// タイムゾーンを東京標準に設定する
+// date_default_timezone_set('Asia/Tokyo');
+// print (time());
+print (date('n/j(D)', time() + 60 * 60 * 24));
+?>
+</pre>
+</main>
+</body>
+</html>
