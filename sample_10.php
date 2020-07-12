d27703857173c009a554653b21f75a4ea6a5b755
sato <kujira028@gmail.com>
2020-07-12 14:57:44 +0900

    [add]if構文について

---
+++ sample_10.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+// タイムゾーンを東京標準に設定する
+date_default_timezone_set('Asia/Tokyo');
+print(date('G'));
+
+// 9時開店のお店なのでそれ以前は「受付時間外」と表示させる
+if (date('G') < 9){
+print('※ 現在受付時間外です');
+} else {
+  print('ようこそ');
+}
+
+// if (date('G') >= 9){
+//   print('ようこそ');
+// }
+?>
+</pre>
+</main>
+</body>
+</html>
